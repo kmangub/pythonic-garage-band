109ed9cde62d35368a43c4d41f30f7e744074def
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2020-12-16 19:40:08 -0800

    finished tests 1 through 7

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,37 @@
+class Band:
+    def __init__(self, name, members=None):
+        self.name = name
+        self.members = members
+
+    def __str__(self):
+        return f"The band {self.name}"
+
+    def __repr__(self):
+        return f"Band instance. name={self.name}, members={self.members}"
+
+
+class Musician:
+    pass
+
+class Guitarist:
+    def __init__(self, name='Random Name'):
+        self.name = name
+        
+    def __str__(self):
+        return f'My name is {self.name} and I play guitar'
+
+    def __repr__(self):
+        return f'Guitarist instance. Name = {self.name}'
+class Bassist:
+    pass
+
+class Drummer:
+    def __init__(self, name = 'Random Name'):
+        self.name = name
+    
+    def __str__(self):
+        return f'My name is {self.name} and I play drums'
+
+    def __repr__(self):
+        return f'Drummer instance. Name = {self.name}'
+    
